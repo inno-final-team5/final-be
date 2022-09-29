8b68db0b668399a9acd8d5e1c22b8a971115238a
petal416@naver.com <petal416@naver.com>
2022-09-29 16:02:22 +0900

    security configuration 수정

--- src/main/java/com/sparta/innovationfinal/config/SecurityConfiguration.java
+++ src/main/java/com/sparta/innovationfinal/config/SecurityConfiguration.java
@@ -52,7 +52,7 @@ public class SecurityConfiguration {
 
                 .and()
                 .authorizeRequests()
-                .antMatchers("/api/auth").authenticated()
+                .antMatchers("/auth").authenticated()
                 .anyRequest().permitAll()
                 // JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스를 적용
                 .and()
