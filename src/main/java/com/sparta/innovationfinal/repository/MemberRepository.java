ee96a071e402c20b731a3f31e5ace8793c600d9f
petal416@naver.com <petal416@naver.com>
2022-10-07 09:48:40 +0900

    test

--- src/main/java/com/sparta/innovationfinal/repository/MemberRepository.java
+++ src/main/java/com/sparta/innovationfinal/repository/MemberRepository.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
- 
+
     Optional<Member> findByEmail(String email);
     Optional<Member> findByNickname(String nickname);
     Member findMemberByEmail(String email);
