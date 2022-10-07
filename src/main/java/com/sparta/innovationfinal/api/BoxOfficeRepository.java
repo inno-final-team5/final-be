0305ba4aee40b5ad75206e99775f2e5be1b3f821
petal416@naver.com <petal416@naver.com>
2022-10-07 09:57:57 +0900

    test

--- src/main/java/com/sparta/innovationfinal/api/BoxOfficeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sparta.innovationfinal.api;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BoxOfficeRepository extends JpaRepository<ExcelData,Long> {
-
-}
