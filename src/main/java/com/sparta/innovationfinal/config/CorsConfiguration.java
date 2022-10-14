8f0386dabc36fbfd35edd7e64c8789ca00c29e0b
Jei-you <jeiyou_kc@naver.com>
2022-10-15 00:19:04 +0900

    Update CorsConfiguration.java

--- src/main/java/com/sparta/innovationfinal/config/CorsConfiguration.java
+++ src/main/java/com/sparta/innovationfinal/config/CorsConfiguration.java
@@ -11,7 +11,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://www.moviecritic.site", "*")
+                .allowedOrigins("*")
                 .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE", "HEAD")
                 .allowedHeaders("*")
                 .exposedHeaders("*")
