bf13d7faa0f2093242f307abb34445ed66550da7
petal416@naver.com <petal416@naver.com>
2022-10-17 17:05:49 +0900

    카카오 로그인 결과값에 mainBadge 추가

--- src/main/java/com/sparta/innovationfinal/OAuth/OAuthResponseDto.java
+++ src/main/java/com/sparta/innovationfinal/OAuth/OAuthResponseDto.java
@@ -13,6 +13,7 @@ public class OAuthResponseDto {
     private String accessToken;
     private String refreshToken;
     private String kakaoToken;
+    private Long mainBadge;
 
     public OAuthResponseDto(Member kakaoUser, TokenDto tokenDto, String kakaoToken) {
         this.email = kakaoUser.getEmail();
@@ -20,5 +21,7 @@ public class OAuthResponseDto {
         this.accessToken = tokenDto.getAccessToken();
         this.refreshToken = tokenDto.getRefreshToken();
         this.kakaoToken = kakaoToken;
+        this.mainBadge = kakaoUser.getMainBadge();
     }
+
 }
