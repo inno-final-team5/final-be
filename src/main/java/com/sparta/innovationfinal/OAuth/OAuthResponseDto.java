6c725a927cba8223770e724f6c5f0b9046b63fad
petal416@naver.com <petal416@naver.com>
2022-10-17 17:27:21 +0900

    badgeId 수정

--- src/main/java/com/sparta/innovationfinal/OAuth/OAuthResponseDto.java
+++ src/main/java/com/sparta/innovationfinal/OAuth/OAuthResponseDto.java
@@ -13,7 +13,7 @@ public class OAuthResponseDto {
     private String accessToken;
     private String refreshToken;
     private String kakaoToken;
-    private Long mainBadge;
+    private Long badgeId;
 
     public OAuthResponseDto(Member kakaoUser, TokenDto tokenDto, String kakaoToken) {
         this.email = kakaoUser.getEmail();
@@ -21,7 +21,7 @@ public class OAuthResponseDto {
         this.accessToken = tokenDto.getAccessToken();
         this.refreshToken = tokenDto.getRefreshToken();
         this.kakaoToken = kakaoToken;
-        this.mainBadge = kakaoUser.getMainBadge();
+        this.badgeId = kakaoUser.getMainBadge();
     }
 
 }
